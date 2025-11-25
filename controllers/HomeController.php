19b7b27e4a656ec4f420fb6ce4df5d3894f5b69e
Mohamed Nasr <mohamednasrmahmood@gmail.com>
2025-11-25 23:37:05 +0200

    Implement Show data analysis feature

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -4,61 +4,60 @@
 namespace App\Controllers;
 use App\Core\View;
 use App\Models\Transaction;
+
+use function PHPSTORM_META\expectedReturnValues;
+
 class HomeController
 {
-        public function index(): string
-        {
-            echo "hello from home controller index method";
+    public function index(): string
+    {
+        echo "hello from home controller index method";
 
-           return View::make("Home/index");
-        }
+        return View::make("Home/index");
+    }
 
-        public function create()
-        {
-            echo "hello from home controller create method";
-        }
+    public function create()
+    {
+        echo "hello from home controller create method";
+    }
 
-        public function upload()
-        {
-            $from = $_FILES["csvFile"]["tmp_name"];
-            $to = UPLOADS_PATH . $_FILES["csvFile"]["name"];
+    public function upload()
+    {
+        $from = $_FILES["csvFile"]["tmp_name"];
+        $to = UPLOADS_PATH . $_FILES["csvFile"]["name"];
+        move_uploaded_file($from, $to);
+        $_SESSION["msg"] = "File uploaded successfully!";
+        $this->processCSV($to);
+        return header("Location: /home");
+    }
+    public function show()
+    {
+        // $this->processCSV(UPLOADS_PATH . $_FILES["csvFile"]["name"]);
 
-            
-            move_uploaded_file($from, $to);
-            $_SESSION["msg"] = "File uploaded successfully!";
-            
-            $this->processCSV($to);
-            // http_response_code(200);
-            return header("Location: /home");    
-        }
-        public function show()
-        {
-            // $this->processCSV(UPLOADS_PATH . $_FILES["csvFile"]["name"]);
-            
-        }
+    }
 
-        private function processCSV($filePath)
-        {
-        
+    private function processCSV($filePath): string
+    {
 
-            // Implement CSV processing logic here
-            $file = fopen($filePath, 'r');
-            $transactions =[];
-            while (($data = fgetcsv($file)) !== FALSE) {
-                
-                // Process each row of the CSV
-                // echo "<pre>";
-                // print_r($data);
-                // echo "</pre>";
-                $transactions[] = $data;
-            }
-            // print_r($transactions);
-            fclose($file);
-            array_shift($transactions);
-            $transactionModel=new Transaction();
 
-            $transactionModel->store($transactions);
-            
-        
-        }
+        $transactionModel = new Transaction();
+        $file = fopen($filePath, 'r');
+        $transactions = $keys = [];
+
+        [$transactions, $keys] = $transactionModel->readData($file, $transactions, $keys);
+
+        [$income, $expanse, $netTotal] = $transactionModel->prepareStatsToView($transactions);
+
+        $data = $transactionModel->prepareDataToView($transactions);
+
+        var_dump($data, $keys, $income, $expanse, $netTotal);
+
+        // return View::make("Home/show", [
+        //     "data" => $data,
+        //     "keys" => $keys,
+        //     "income" => $income,
+        //     "expanse" => $expanse,
+        //     "netTotal" => $netTotal
+        // ]);
+    }
 }
--- models/Transaction.php
+++ models/Transaction.php
@@ -17,11 +17,11 @@ class Transaction extends BaseModel
         'description',
         'amount',
     ];
-    
+
     public function __construct(array $attributes = [])
     {
         parent::__construct(new \App\Core\Mysql());
-        
+
         foreach ($attributes as $key => $value) {
             if (in_array($key, $this->fillable)) {
                 $this->$key = $value;
@@ -29,6 +29,10 @@ class Transaction extends BaseModel
         }
     }
 
+    public function processData(): void
+    {
+
+    }
 
     public function Store(array $data)
     {
@@ -36,14 +40,95 @@ class Transaction extends BaseModel
         $sql = "INSERT INTO {$this->table} ({$fields}) VALUES ( ?, ? , ? ,? )";
         $stmt = $this->db->prepare($sql);
         foreach ($data as $item) {
-            $check = (int)$item[1] ?? 1;
-            $stmt->execute([date("Y-m-d", strtotime($item[0])), $check , $item[2], (int)$this->sentize($item[3])   ]);
+            $check = (int) $item[1] ?? 1;
+            $stmt->execute([date("Y-m-d", strtotime($item[0])), $check, $item[2], (int) $this->sentize($item[3])]);
         }
 
     }
-    private function sentize($val)  {
-        return str_replace("$" , "" , $val);
+    private function sentize($val)
+    {
+        return str_replace("$", "", $val);
     }
-    
-    
+
+    private function getIncome(array $data): float
+    {
+        return
+            array_reduce(
+                $data,
+                fn($sum, $item) => $sum + (float) ($item[3] >= 0 ? $item[3] : 0)
+            );
+
+    }
+
+    private function getExpance(array $data): float
+    {
+
+        return
+            array_reduce(
+                $data,
+                fn($sum, $item) => $sum + (float) ($item[3] < 0 ? $item[3] : 0)
+            );
+
+    }
+
+    private function getNetTotal(float $income, float $expance): float
+    {
+        return $income + $expance;
+
+    }
+
+    private function getStats($data): array
+    {
+        $income = $this->getIncome($data);
+
+        $expance = $this->getExpance($data);
+
+        $netTotal = $this->getNetTotal($income, $expance);
+
+        return [$income, $expance, $netTotal];
+    }
+    function prepareDataToView($data)
+    {
+        // prepare Data transactions
+        $data = array_map(function ($item) {
+
+            // Prepare Date to show
+            $item[0] = date("M d ,Y", strtotime($item[0]));
+
+            //prepare amount values
+            $item[3] = str_replace(",", "", $item[3]);
+            $item[3] = floatval($item[3]);
+            $item[3] = ($item[3] > 0 ? "+ $" . number_format($item[3], 2) : "- $" . number_format($item[3] * -1, 2));
+
+            return $item;
+        }, $data);
+
+        return $data;
+    }
+
+    function prepareStatsToView($stats)
+    {
+                $stats = $this->getStats($stats);
+
+        $stats = array_map(function ($item) {
+            $item = ($item > 0 ? "+ $" . number_format($item, 2) : "- $" . number_format($item * -1, 2));
+            return $item;
+        }, $stats);
+
+        return $stats;
+    }
+
+    function readData($file_stream, $data, $keys)
+    {
+        while (($record = fgetcsv($file_stream)) !== false) {
+
+            //  To ignore record one
+            if ($record[3][0] === "$" || $record[3][0] === "-") {
+                $record[3] = $this->sentize($record[3]);
+                array_push($data, $record);
+            } else
+                array_push($keys, ...$record);
+        }
+        return [$data, $keys];
+    }
 }
